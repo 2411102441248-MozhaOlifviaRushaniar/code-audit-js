f1d8754dd14566d71e290a2d8444d62330754203
2411102441248-MozhaOlifviaRushaniar <2411102441248@umkt.ac.id>
2026-04-14 11:56:54 +0800

    Initial commit - original code

---
+++ program.js
@@ -0,0 +1,12 @@
+// Menghitung sesuatu
+function htg(p, l) {
+    let r = p * l;
+    console.log("Hasilnya adalah: " + r);
+    
+    // Simpan ke database (ceritanya)
+    console.log("Data disimpan...");
+    
+    return r;
+}
+
+htg(10, 5);
