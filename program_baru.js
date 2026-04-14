96aae72fb989006a908a3fd2adb66f51a4749d06
2411102441248-MozhaOlifviaRushaniar <2411102441248@umkt.ac.id>
2026-04-14 11:57:50 +0800

    Refactor code using clean code principles

---
+++ program_baru.js
@@ -0,0 +1,26 @@
+// Menghitung luas persegi panjang
+function hitungLuas(panjang, lebar) {
+    return panjang * lebar;
+}
+
+// Menampilkan hasil ke console
+function tampilkanHasil(luas) {
+    console.log("Hasilnya adalah: " + luas);
+}
+
+// Simulasi menyimpan data
+function simpanData() {
+    console.log("Data disimpan...");
+}
+
+// Program utama
+function main() {
+    const panjang = 10;
+    const lebar = 5;
+
+    const luas = hitungLuas(panjang, lebar);
+    tampilkanHasil(luas);
+    simpanData();
+}
+
+main();
